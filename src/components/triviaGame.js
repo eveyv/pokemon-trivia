ffd6e24b60d6d4a9ae079d6922df48994f988a8a
Everett Yeaw <evyeaw@gmail.com>
2020-10-30 14:49:37 -0400

    questions render properly- logic needs tweaking for questionNumber

--- src/components/triviaGame.js
+++ src/components/triviaGame.js
@@ -1,43 +1,62 @@
 import React, { Component } from 'react';
-import TriviaQuestions from '../questions'
+import Questions from '../data.json'
 
 class TriviaGame extends Component {
 
-  state = {
-    triviaStart: false,
-    triviaReset: false,
-    totalCorrect: 0,
-    questionNumber: 0,
-    totalQuestions: 0,
-    question: '',
-    answerOne: '',
-    answerTwo: '',
-    answerThree: '',
-    answerFour: '',
-    correct: '',
-  };
+      state = {
+        triviaStart: false,
+        triviaReset: false,
+        totalCorrect: 0,
+        questionNumber: '',
+        totalQuestions: 0,
+        question: '',
+        answerOne: '',
+        answerTwo: '',
+        answerThree: '',
+        answerFour: '',
+        correct: '',
+      };
 
-  newQuestion = () => {
-    if (this.state.triviaStart === true) {
+
+
+    newQuestion = () => {
+      if (this.state.triviaStart === true) {
+        this.setState({
+          question: Questions[this.state.questionNumber].question,
+          answerOne: Questions[this.state.questionNumber].answerOne,
+          answerTwo: Questions[this.state.questionNumber].answerTwo,
+          answerThree: Questions[this.state.questionNumber].answerThree,
+          answerFour: Questions[this.state.questionNumber].answerFour,
+          correct: Questions[this.state.questionNumber].correct,
+          totalQuestions: Questions.length
+        });
+      }
+    };
+
+    triviaGameStart = () => {
       this.setState({
-        question: Questions[this.state.questionNumber].question,
-        answerOne: Questions[this.state.questionNum].answerOne,
-        answerTwo: Questions[this.state.questionNum].answerTwo,
-        answerThree: Questions[this.state.questionNum].answerThree,
-        answerFour: Questions[this.state.questionNum].answerFour,
-        correct: Questions[this.state.questionNum].correct,
-        totalQuestions: Questions.length
+        triviaStart: true,
+        questionNumber: this.state.questionNumber = 1,
       })
-    }
-  };
+      this.newQuestion();
+    };
 
-  triviaGameStart = () => {
-    this.setState({
-      triviaStart: true,
-      questionNumber: this.state.questionNumber + 1
-    })
-    this.newQuestion()
-  };
+      render() {
+        return(
+          <div>
+            <button className="play-buttom" onClick={this.triviaGameStart}>
+              Play
+            </button>
+            <h3 className='question-number'>
+              Question {this.state.questionNumber}
+            </h3>
+            <h2 className='trivia-question'>
+              {this.state.question}
+            </h2>
+
+          </div>
+        )
+      }
 };
 
 export default TriviaGame;
