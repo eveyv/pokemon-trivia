9f32e380de68f01b3f9f5b444ccd1d4639805bbd
Everett Yeaw <evyeaw@gmail.com>
2020-10-30 21:13:18 -0400

    answers rendering albeit improperly

--- src/components/triviaGame.js
+++ src/components/triviaGame.js
@@ -7,8 +7,8 @@ class TriviaGame extends Component {
         triviaStart: false,
         triviaReset: false,
         totalCorrect: 0,
-        questionNumber: '',
-        totalQuestions: 0,
+        questionNumber: 0,
+        totalQuestions: 10,
         question: '',
         answerOne: '',
         answerTwo: '',
@@ -16,8 +16,6 @@ class TriviaGame extends Component {
         answerFour: '',
         correct: '',
       };
-
-
 
     newQuestion = () => {
       if (this.state.triviaStart === true) {
@@ -28,7 +26,6 @@ class TriviaGame extends Component {
           answerThree: Questions[this.state.questionNumber].answerThree,
           answerFour: Questions[this.state.questionNumber].answerFour,
           correct: Questions[this.state.questionNumber].correct,
-          totalQuestions: Questions.length
         });
       }
     };
@@ -36,27 +33,76 @@ class TriviaGame extends Component {
     triviaGameStart = () => {
       this.setState({
         triviaStart: true,
-        questionNumber: this.state.questionNumber = 1,
       })
       this.newQuestion();
     };
 
-      render() {
-        return(
-          <div>
-            <button className="play-buttom" onClick={this.triviaGameStart}>
-              Play
-            </button>
-            <h3 className='question-number'>
-              Question {this.state.questionNumber}
-            </h3>
-            <h2 className='trivia-question'>
-              {this.state.question}
-            </h2>
+    checkAnswer = e => {
+      this.setState({
+        questionNumber: this.state.questionNumber + 1
+      });
+      this.otherCheck(e);
+    };
 
-          </div>
-        )
-      }
+    otherCheck = e => {
+      let id = e.target.getAttribute('dataId');
+        if (id === this.state.correct) {
+          this.setState({
+            totalCorrect: this.state.totalCorrect + 1
+          });
+        }
+          console.log('questionNumber', this.state.questionNumber);
+        if (this.state.questionNumber < this.state.totalQuestions) {
+          this.newQuestion();
+        } else {
+          // game over
+        }
+      };
+
+  render() {
+    return(
+      <div>
+      <header>
+        Score: {this.state.totalCorrect}
+      </header>
+        <button className="button" onClick={this.triviaGameStart}>
+          Play
+        </button>
+        <h3 className='question-number'>
+          Question {this.state.totalQuestions}
+        </h3>
+        <h2 className='trivia-question'>
+          {this.state.question}
+        </h2>
+          <ul className="answers">
+            <button
+              className="answer-one"
+              onClick={this.checkAnswer}
+              dataId='1'>
+                {this.state.answerOne}
+            </button>
+            <button
+              className="answer-two"
+              onClick={this.checkAnswer}
+              dataId="2">
+                {this.state.answerTwo}
+            </button>
+            <button
+              className="answer-three"
+              onClick={this.checkAnswer}
+              dataId="3">
+                {this.state.answerThree}
+            </button>
+            <button
+              className="answer-four"
+              onClick={this.checkAnswer}
+              dataId="4">
+                {this.state.answerFour}
+            </button>
+          </ul>
+      </div>
+    )
+  }
 };
 
 export default TriviaGame;
