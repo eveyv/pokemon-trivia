9cd12cccd4c4c596e619d00f1218d76ff6388559
Everett Yeaw <evyeaw@gmail.com>
2020-10-30 21:42:27 -0400

    play button fixed; first onclick still needs 2x

--- src/components/triviaGame.js
+++ src/components/triviaGame.js
@@ -4,7 +4,6 @@ import Questions from '../data.json'
 class TriviaGame extends Component {
 
       state = {
-        triviaStart: false,
         triviaReset: false,
         totalCorrect: 0,
         questionNumber: 0,
@@ -18,7 +17,6 @@ class TriviaGame extends Component {
       };
 
     newQuestion = () => {
-      if (this.state.triviaStart === true) {
         this.setState({
           question: Questions[this.state.questionNumber].question,
           answerOne: Questions[this.state.questionNumber].answerOne,
@@ -27,31 +25,26 @@ class TriviaGame extends Component {
           answerFour: Questions[this.state.questionNumber].answerFour,
           correct: Questions[this.state.questionNumber].correct,
         });
-      }
     };
 
     triviaGameStart = () => {
-      this.setState({
-        triviaStart: true,
-      })
       this.newQuestion();
     };
 
-    checkAnswer = e => {
+    isCorrect = e => {
       this.setState({
         questionNumber: this.state.questionNumber + 1
       });
-      this.otherCheck(e);
+      this.idCheck(e);
     };
 
-    otherCheck = e => {
+    idCheck = e => {
       let id = e.target.getAttribute('dataId');
         if (id === this.state.correct) {
           this.setState({
             totalCorrect: this.state.totalCorrect + 1
           });
         }
-          console.log('questionNumber', this.state.questionNumber);
         if (this.state.questionNumber < this.state.totalQuestions) {
           this.newQuestion();
         } else {
@@ -62,44 +55,42 @@ class TriviaGame extends Component {
   render() {
     return(
       <div>
-      <header>
-        Score: {this.state.totalCorrect}
-      </header>
-        <button className="button" onClick={this.triviaGameStart}>
-          Play
-        </button>
-        <h3 className='question-number'>
-          Question {this.state.totalQuestions}
-        </h3>
-        <h2 className='trivia-question'>
-          {this.state.question}
-        </h2>
-          <ul className="answers">
+        <header>
+          Score: {this.state.totalCorrect}
+        </header>
+          <button className="button" onClick={this.triviaGameStart}>
+            Play
+          </button>
+          <h3 className='question-number'>
+            Question {this.state.totalQuestions}
+          </h3>
+          <h2 className='trivia-question'>
+            {this.state.question}
+          </h2>
             <button
               className="answer-one"
-              onClick={this.checkAnswer}
+              onClick={this.isCorrect}
               dataId='1'>
                 {this.state.answerOne}
             </button>
             <button
               className="answer-two"
-              onClick={this.checkAnswer}
+              onClick={this.isCorrect}
               dataId="2">
                 {this.state.answerTwo}
             </button>
             <button
               className="answer-three"
-              onClick={this.checkAnswer}
+              onClick={this.isCorrect}
               dataId="3">
                 {this.state.answerThree}
             </button>
             <button
               className="answer-four"
-              onClick={this.checkAnswer}
+              onClick={this.isCorrect}
               dataId="4">
                 {this.state.answerFour}
             </button>
-          </ul>
       </div>
     )
   }
